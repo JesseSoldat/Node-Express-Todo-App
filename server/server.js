811429f8d37200ae6e9fc738e87cfc810c66f473
JesseSoldat <jsoldat@hotmail.com>
2018-01-20 21:48:01 +0700

    ready to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
+require('./config/config');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const { mongoose } = require('./db/mongoose');
 const app = express();
 const port = process.env.PORT;
 
